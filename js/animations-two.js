415b7a372a56b48dad87f01ee3b682050acc6003
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-04-06 16:41:18 +0200

    speed animation

--- js/animations-two.js
+++ js/animations-two.js
@@ -2,10 +2,10 @@ document.querySelector('.player').addEventListener('click', runForestRun);
 var one = document.querySelector('.one');
 var two = document.querySelector('.two');
 
-let one_width = 10,two_width = 5;
-let one_height = 10,two_height = 5;
-let one_margin_top = 45,two_margin_top = 47.5;
-let one_left = 45,two_left = 47.5;
+let one_width = 10,two_width = 10;
+let one_height = 10,two_height = 10;
+let one_margin_top = 45,two_margin_top = 55;
+let one_left = 45,two_left = 55;
 
 function runForestRun(){
   console.log(one_width)
@@ -18,6 +18,10 @@ function runForestRun(){
     one.style.height = one_height + '%';
     one.style.top = one_margin_top + '%';
     one.style.left = one_left + '%';
+    two.style.width = (one_width-10) + '%';
+    two.style.height = (one_height-10) + '%';
+    two.style.top = (one_margin_top+5) + '%';
+    two.style.left = (one_left+5) + '%';
   }else{
     one_width = 20;
     one_height = 20;
