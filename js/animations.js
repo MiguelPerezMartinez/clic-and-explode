712785094a554c469a5aa2c2d68496c33d6f9c94
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-04-06 22:16:28 +0200

    end animations

--- js/animations.js
+++ js/animations.js
@@ -1,150 +1,103 @@
+document.querySelector('.player').addEventListener('click', runForestRun);
+var one = document.querySelector('.one');
+var three = document.querySelector('.three');
 
+var deep_one = document.querySelector('.deep-one');
+var deep_two = document.querySelector('.deep-two');
 
-//Button explode animation inflate when clic
-document.querySelector('.red').addEventListener('click',buttonExplodeRed);
-document.querySelector('.green').addEventListener('click',buttonExplodeGreen);
-document.querySelector('.blue').addEventListener('click',buttonExplodeBlue);
-document.querySelector('.purple').addEventListener('click',buttonExplodePurple);
-document.querySelector('.orange').addEventListener('click',buttonExplodeOrange);
-document.querySelector('.yellow').addEventListener('click',buttonExplodeYellow);
-var explodeBttnRed = document.querySelector('.red');
-var explodeBttnGreen = document.querySelector('.green');
-var explodeBttnBlue = document.querySelector('.blue');
-var explodeBttnPurple = document.querySelector('.purple');
-var explodeBttnOrange = document.querySelector('.orange');
-var explodeBttnYellow = document.querySelector('.yellow');
+let one_width = 5,two_width = 5,three_width = 5,four_width = 5;
+let one_height = 5,two_height = 5,three_height = 5,four_height = 5;
+let one_margin_top = 47.5,two_margin_top = 55,three_margin_top = 47.5,four_margin_top = 55;
+let one_left = 47.5,two_left = 55,three_left = 47.5,four_left = 55;
 
-//Tunnel animation
-var explodeMarcoUno = document.querySelector('.marco-uno');
+let next = false;
 
-let r_width = 20,g_width = 20,b_width = 20,p_width = 20,o_width = 20,y_width = 20;
-let r_height = 20,g_height = 20,b_height = 20,p_height = 20,o_height = 20,y_height = 20;
-let r_margin_top = 40,g_margin_top = 40,b_margin_top = 40,p_margin_top = 40,o_margin_top = 40,y_margin_top = 40;
-let r_left = 40,g_left = 40,b_left = 40,p_left = 40,o_left = 40,y_left = 40;
+function runForestRun(){
 
+  psycho();
 
-function buttonExplodeRed(){
-  console.log(r_width);
-  if(r_width<100){
-    r_width += 10;
-    r_height += 10;
-    r_margin_top -= 5;
-    r_left -= 5;
-    explodeBttnRed.style.width = r_width + '%';
-    explodeBttnRed.style.height = r_height + '%';
-    explodeBttnRed.style.top = r_margin_top + '%';
-    explodeBttnRed.style.left = r_left + '%';
+  psychoEnd();
+
+  if(one_width<95){
+    one_width += 5;
+    one_height += 5;
+    one_margin_top -= 2.5;
+    one_left -= 2.5;
+    one.style.width = one_width + '%';
+    one.style.height = one_height + '%';
+    one.style.top = one_margin_top + '%';
+    one.style.left = one_left + '%';
+    if(one_width>45){
+      next = true;
+    };
+    if(next == true){
+      if(three_width<95){
+        three_width += 5;
+        three_height += 5;
+        three_margin_top -= 2.5;
+        three_left -= 2.5;
+        three.style.width = three_width + '%';
+        three.style.height = three_height + '%';
+        three.style.top = three_margin_top + '%';
+        three.style.left = three_left + '%';
+      }else{
+        three_width = 5;
+        three_height = 5;
+        three_margin_top = 47.5;
+        three_left = 47.5;
+        three.style.display = 'none';
+        three.style.display = 'block';
+      };
+    };
   }else{
-    r_width = 20;
-    r_height = 20;
-    r_margin_top = 40;
-    r_left = 40;
-    explodeBttnRed.style.display = 'none';
-    explodeBttnGreen.style.display = 'block';
+    one_width = 5;
+    one_height = 5;
+    one_margin_top = 47.5;
+    one_left = 47.5;
+    one.style.display = 'none';
+    one.style.display = 'block';
   };
+}
+
+function psycho(){
+  deep_one.style.transform += 'rotate(25deg)';
+  deep_two.style.transform += 'rotate(15deg)';
+
+  let psycho_one = document.querySelector('.psycho-one');
+  let psycho_two = document.querySelector('.psycho-two');
+  let psycho_three = document.querySelector('.psycho-three');
+  let psycho_four = document.querySelector('.psycho-four');
+
+  let x = parseInt((Math.random() * 95) + 1);
+  let y = parseInt((Math.random() * 95) + 1);
+
+  if(parseInt(Math.random() * 2) == true){
+    psycho_one.style.top = x + '%';
+    psycho_one.style.left = y + '%';
+  }
+
+  if(parseInt(Math.random() * 2) == true){
+    psycho_two.style.top = x + '%';
+    psycho_two.style.left = y + '%';
+  }
+
+  if(parseInt(Math.random() * 2) == true){
+    psycho_three.style.top = x + '%';
+    psycho_three.style.left = y + '%';
+  }
+
+  if(parseInt(Math.random() * 2) == true){
+    psycho_four.style.top = x + '%';
+    psycho_four.style.left = y + '%';
+  }
 };
 
-function buttonExplodeGreen(){
-  console.log(g_width);
-  if(g_width<100){
-    g_width += 10;
-    g_height += 10;
-    g_margin_top -= 5;
-    g_left -= 5;
-    explodeBttnGreen.style.width = g_width + '%';
-    explodeBttnGreen.style.height = g_height + '%';
-    explodeBttnGreen.style.top = g_margin_top + '%';
-    explodeBttnGreen.style.left = g_left + '%';
-  }else{
-    g_width = 20;
-    g_height = 20;
-    g_margin_top = 40;
-    g_left = 40;
-    explodeBttnGreen.style.display = 'none';
-    explodeBttnBlue.style.display = 'block';
-  };
-};
-
-function buttonExplodeBlue(){
-  console.log(b_width);
-  if(b_width<100){
-    b_width += 10;
-    b_height += 10;
-    b_margin_top -= 5;
-    b_left -= 5;
-    explodeBttnBlue.style.width = b_width + '%';
-    explodeBttnBlue.style.height = b_height + '%';
-    explodeBttnBlue.style.top = b_margin_top + '%';
-    explodeBttnBlue.style.left = b_left + '%';
-  }else{
-    b_width = 20;
-    b_height = 20;
-    b_margin_top = 40;
-    b_left = 40;
-    explodeBttnBlue.style.display = 'none';
-    explodeBttnPurple.style.display = 'block';
-  };
-};
-
-function buttonExplodePurple(){
-  console.log(p_width);
-  if(p_width<100){
-    p_width += 10;
-    p_height += 10;
-    p_margin_top -= 5;
-    p_left -= 5;
-    explodeBttnPurple.style.width = p_width + '%';
-    explodeBttnPurple.style.height = p_height + '%';
-    explodeBttnPurple.style.top = p_margin_top + '%';
-    explodeBttnPurple.style.left = p_left + '%';
-  }else{
-    p_width = 20;
-    p_height = 20;
-    p_margin_top = 40;
-    p_left = 40;
-    explodeBttnPurple.style.display = 'none';
-    explodeBttnOrange.style.display = 'block';
-  };
-};
-
-function buttonExplodeOrange(){
-  console.log(o_width);
-  if(o_width<100){
-    o_width += 10;
-    o_height += 10;
-    o_margin_top -= 5;
-    o_left -= 5;
-    explodeBttnOrange.style.width = o_width + '%';
-    explodeBttnOrange.style.height = o_height + '%';
-    explodeBttnOrange.style.top = o_margin_top + '%';
-    explodeBttnOrange.style.left = o_left + '%';
-  }else{
-    o_width = 20;
-    o_height = 20;
-    o_margin_top = 40;
-    o_left = 40;
-    explodeBttnOrange.style.display = 'none';
-    explodeBttnYellow.style.display = 'block';
-  };
-};
-
-function buttonExplodeYellow(){
-  console.log(y_width);
-  if(y_width<100){
-    y_width += 10;
-    y_height += 10;
-    y_margin_top -= 5;
-    y_left -= 5;
-    explodeBttnYellow.style.width = y_width + '%';
-    explodeBttnYellow.style.height = y_height + '%';
-    explodeBttnYellow.style.top = y_margin_top + '%';
-    explodeBttnYellow.style.left = y_left + '%';
-  }else{
-    y_width = 20;
-    y_height = 20;
-    y_margin_top = 40;
-    y_left = 40;
-    explodeBttnYellow.style.display = 'none';
-    explodeBttnRed.style.display = 'block';
-  };
-};
+function psychoEnd(){
+  document.querySelector('.player').style.backgroundColor = 'white';
+  document.querySelector('.player').style.transform = 'skewY(5rad)';
+  document.querySelector('.player').style.width = '100%';
+  document.querySelector('.player').style.height = '100%';
+  document.querySelector('.player').style.top = 0;
+  document.querySelector('.player').style.left = 0;
+  document.querySelector('.player').style.transform = 'skewY(10rad)';
+}
